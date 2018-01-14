7d2de112a21d81dcfcba120ffc7bfd8d025bd41e
Mario Linz <gelight@gmx.de>
2018-01-14 22:12:58 +0100

    First successed test together with ember.

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
+// Setup cors
+// restify.CORS.ALLOW_HEADERS.push('accept');
+// restify.CORS.ALLOW_HEADERS.push('sid');
+// restify.CORS.ALLOW_HEADERS.push('lang');
+// restify.CORS.ALLOW_HEADERS.push('origin');
+// restify.CORS.ALLOW_HEADERS.push('withcredentials');
+// restify.CORS.ALLOW_HEADERS.push('x-requested-with');
+// server.use(restify.CORS());
+// server.use(restify.CORS({
+//     'origins': ['http://localhost:4200']
+// }));
+
 // var config = require('./resources/index');
 
 // 
@@ -22,7 +34,8 @@ server.get('/api/activities', function (req, res, next) {
                 id: 1,
                 version: 1,
                 active: true,
-                date: null,
+                //categories: [1,3,6],
+                date: 'Sun Jan 14 2018 22:09:36 GMT+0100 (Mitteleuropäische Zeit)',
                 title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quae cum essent dicta, finem fecimus et ambulandi et disputandi. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Quod ea non occurrentia fingunt, vincunt Aristonem; Non enim solum Torquatus dixit quid sentiret, sed etiam cur. Sed ad bona praeterita redeamus.",
                 img: "http://www.hltretz.ac.at/eventplanet/wp-content/uploads/2015/12/Event-management-post-1.png",
